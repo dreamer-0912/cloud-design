e8c1e3e90caffe0d71807d91eda88e56e1857223
yin.jie <997469268@qq.com>
2025-07-17 18:49:52 +0800

    更新button样式

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -4,12 +4,10 @@
 
 <template>
   <div class="container">
-    <cloud-button></cloud-button>
+    <cloud-button size="mini" round disabled></cloud-button>
+    <cloud-button size="mini" type="primary"></cloud-button>
+    <cloud-button size="mini" type="success" disabled></cloud-button>
+    <cloud-button size="mini" type="danger" round disabled></cloud-button>
+    <cloud-button size="mini" type="warning" disabled></cloud-button>
   </div>
 </template>
-
-<style>
-.container {
-  background: hsl(from var(--test-background) h s calc(l + 20));
-}
-</style>
--- examples/src/main.js
+++ examples/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 // 全局注册
 import CloudComponents from '../../packages/index.js'
-
+import '@cloud-design/theme/src/index.scss'
 const app = createApp(App)
 app.use(CloudComponents)
 app.mount('#app')
